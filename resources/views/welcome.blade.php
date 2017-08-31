87091e66232e17c4d416cff05e5b7437c729be4d
huibo <huibo@ihuibo.cn>
2017-08-31 21:26:57 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel惠博
+                     Hello Laravel
                 </div>
 
                 <div class="links">
